df178e8e872c460b3ad3ce8f7494fe44d69c9489
Esger <esgerj@gmail.com>
2025-11-21 01:56:41 +0100

    mobile 100%

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-export class App {}
+export class App {
+    attached() {
+        if (this.isMobile()) {
+            document.body.classList.add('mobile-view');
+        }
+    }
+
+    isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+}
